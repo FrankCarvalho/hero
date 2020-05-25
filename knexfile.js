d1f1987c3c70b71ffa5a8e8ddab2129395c90b11
FrankCarvalho <inmob.comercial@gmail.com>
2020-05-25 09:59:29 -0300

    Conexão com banco mysql

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,12 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'mysql',
     connection: {
-      filename: './src/database/db.sqlite'
+    host : '127.0.0.1',
+    user : 'root',
+    password : '',
+    database : 'heropl'
     },
     migrations: {
       directory: './src/database/migrations'
@@ -14,29 +17,14 @@ module.exports = {
   },
 
   test: {
-    client: 'sqlite3',
+    client: 'mysql',
     connection: {
-      filename: './src/database/test.sqlite'
+      filename: './src/database/test.sql'
     },
     migrations: {
       directory: './src/database/migrations'
     },
     useNullAsDefault: true,
-  },
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   },
 
   production: {
